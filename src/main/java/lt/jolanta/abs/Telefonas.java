48ea8c3c74a9c5bf71b663cdf72cff5fbd7489b4
jolan <jolanta.tubutyte@gmail.com>
2023-02-15 11:26:53 +0200

    Add new MainGitDemo class

--- src/main/java/lt/jolanta/abs/Telefonas.java
+++
@@ -1,8 +0,0 @@
-package lt.jolanta.abs;
-
-public interface Telefonas {
-    private String numeris;
-    private String melodija;
-
-    void paskambink(String numeris);
-}
---
+++ src/main/java/lt/jolanta/gitDemo/MainGitDemo.java
@@ -0,0 +1,4 @@
+package lt.jolanta.gitDemo;
+
+public class MainGitDemo {
+}
